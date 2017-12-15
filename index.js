9e9ca2c4d048656159896cde84dff5045c82a437
Joel Mosemann <mosemann@hotmail.com>
2017-12-15 20:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,19 +67,14 @@ function total() {
   
 
 function removeFromCart(item) {
-  let t=0;
-  for (var i = 0, l = cart.length; i < l; i++) {
+   for (var i = 0; i < cart.length; i++) {
      if (cart[i].hasOwnProperty(item)) {
        cart.splice(i, 1);
-       i = (i-1);
-       t=1;
+       return cart;
      }
-  }
-     if (t === 0) {
-       console.log("That item is not in your cart.");
-     }
-     return cart;
-}
+   }
+   return console.log("That item is not in your cart.");
+ }
 
 function placeOrder(cardNumber) {
   // write your code here
