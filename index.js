1d8d93567252a5517abf03df893c33f593937cb2
Joel Mosemann <mosemann@hotmail.com>
2017-12-15 19:58:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,10 +67,15 @@ function total() {
   
 
 function removeFromCart(item) {
+  let t=0
   for (var i = 0, l = cart.length; i < l; i++) {
      if (item === cart[i]) {
        delete cart[i]
        i = (i-1)
+       t=1
+     }
+     if (t=0) {
+       console.log("That item is not in your cart.")
      }
      }
      return cart
